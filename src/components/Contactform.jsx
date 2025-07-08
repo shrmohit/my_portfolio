7363e96e2138729d2d3afaf36f658ab0f53655db
Mohit Sharma <161517182+shrmohit@users.noreply.github.com>
2025-07-08 23:29:54 +0530

    Update Contactform.jsx

--- src/components/Contactform.jsx
+++ src/components/Contactform.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React from 'react';
+
 
 function Contactform() {
   return (
-    <div className='contactform-item'> 
-      
-        <form className='form-section' action="">
-        <h1>Email Me</h1>
-            <input  className="form-input" type="text" placeholder='Your Email' />
-            <input  className="form-input" type="text" placeholder='Your Name' />
-            <input  className="form-input" type="text" placeholder='Subject' />
-            <input className='form-message' type="text" placeholder='Message' />
+    <div className='contact-container'>
+      <div className='contact-section'>
+        <h1>Contact Me</h1>
+        <div className='contactform-item'> 
+          <form className='form-section' action="">
+            <h1>Email Me</h1>
+            <input className="form-input" type="email" placeholder='Your Email' />
+            <input className="form-input" type="text" placeholder='Your Name' />
+            <input className="form-input" type="text" placeholder='Subject' />
+            <textarea className='form-message' placeholder='Message'></textarea>
             <button className='form-inputs'>Send</button>
-        </form>
+          </form>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Contactform
+export default Contactform;
