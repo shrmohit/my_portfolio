e925364fe2228c3f0ee3f8cf07e8bc8b14cb0add
Mohit Sharma <161517182+shrmohit@users.noreply.github.com>
2025-07-08 23:44:46 +0530

    Update constant.js

--- src/data/constant.js
+++ src/data/constant.js
@@ -3,7 +3,8 @@ export const Bio = {
   roles: [
     "Web Developer",
     "Frontend Developer",
-    "UI/UX Designer",
+    "Backend Developer",
+    "Fullstack Developer (MERN)",
     "Programmer",
   ],
   description:
