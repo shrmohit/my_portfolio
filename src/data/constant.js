3d4c5c8ee4307341f1d40ab9a3eaeb9de8e1a758
Mohit Sharma <161517182+shrmohit@users.noreply.github.com>
2025-07-08 23:51:09 +0530

    Update constant.js

--- src/data/constant.js
+++ src/data/constant.js
@@ -11,7 +11,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/shrmohit",
   resume:
-    "https://drive.google.com/file/d/11uX1fsIxfIGyi53rKm65zZmr36mHT_Oy/view?usp=drivesdk",
+    "https://drive.google.com/file/d/1X4-MZWbhS2j-rEUSJzrtIM7LwLvMbLLx/view?usp=drivesdk",
   linkedin: "https://www.linkedin.com/in/mohit-sharma-8210b6215",
   // twitter: "https://twitter.com/RishavChanda",
   // insta: "https://www.instagram.com/rishav_chanda/",
